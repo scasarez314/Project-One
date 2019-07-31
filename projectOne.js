6f52ceb9d7ab17d0e5e3663ce4bd92dc67e922b1
Andrew Pray <a.pray1289@gmail.com>
2019-07-30 19:05:35 -0500

    added ID's and some ajax for api call

--- projectOne.js
+++ projectOne.js
@@ -1,3 +1,26 @@
-alert(
-    "hello world"
-);
+var search = "cheeseburger";
+var queryURL = `https://api.spoonacular.com/recipes/search?apiKey=22453c33f0b040c98babb5d7cfb70d85&query=${search}&information&number=1`
+
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+console.log(response);
+
+})
+
+// on click function 
+
+var searchID = 635350;
+var queryURLId = `https://api.spoonacular.com/recipes/${searchID}/information?apiKey=22453c33f0b040c98babb5d7cfb70d85`;
+
+
+$.ajax({
+    url: queryURLId,
+    method: "GET"
+}).then(function(response){
+console.log(response);
+
+})
