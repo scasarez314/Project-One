c22147bfc3a570026525639a87e94c515ca86039
Dro214TX <valadezpedro214@yahoo.com>
2019-08-02 21:58:24 -0500

    added youtube API

--- projectOne.js
+++ projectOne.js
@@ -2,7 +2,7 @@ $("#search-submit").on("click", function (event) {
     event.preventDefault();
     var search = $("#search-input").val().trim();
     APIrecipe(search);
-})
+});
 
 
 function APIrecipe(search) {
@@ -14,7 +14,7 @@ function APIrecipe(search) {
         console.log(response);
         console.log(search);
 
-        $(".recipe-render").empty();
+        $(".row-recipe-render").empty();
 
         for (var i = 0; i <= response.results.length; i++) {
 
@@ -34,57 +34,79 @@ function APIrecipe(search) {
 
             newCol.append(newCard);
 
-            $(".recipe-render").append(newCol);
+            $(".row-recipe-render").append(newCol);
 
 
         }
     })
-}
+};
 
 // 1: {id: 107878, title: "Garlic Chicken", readyInMinutes: 45, servings: 4, image: "garlic-chicken-2-107878.png", …}
 
-
-
-
-
-
 // on click function 
 
-var searchID = 635350;
-var queryURLId = `https://api.spoonacular.com/recipes/${searchID}/information?apiKey=22453c33f0b040c98babb5d7cfb70d85`;
+// var searchID = 635350;
+// var queryURLId = `https://api.spoonacular.com/recipes/${searchID}/information?apiKey=22453c33f0b040c98babb5d7cfb70d85`;
 
 
-$.ajax({
-    url: queryURLId,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
+// $.ajax({
+//     url: queryURLId,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log(response);
 
-})
+// })
 
-$("#search-submit").on("click", function (event) {
+// $("#search-submit").on("click", function (event) {
+//     event.preventDefault();
+
+//     var searchInput = $("#search-input").val().trim();
+//     $("#search-input").val("");
+//     var newFavorite = $("<div>");
+//     newFavorite.addClass("dropdown-item")
+//     newFavorite.text(searchInput);
+//     newFavorite.appendTo($("#favorite-buttons"));
+
+//     console.log(searchInput);
+
+// });
+
+$(document).on("click", ".my-recipe", function () {
+    // var dynamicText = $(this).text();
+    // var youtube = $("#youtube-input").val() + "recipe";
     event.preventDefault();
+    $(".row-recipe-render").empty();
 
-    var searchInput = $("#search-input").val().trim();
-    $("#search-input").val("");
-    var newFavorite = $("<div>");
-    newFavorite.addClass("dropdown-item")
-    newFavorite.text(searchInput);
-    newFavorite.appendTo($("#favorite-buttons"));
+    var youtube = $("#search-input").val().trim() + "recipe";
+    console.log("#search-input");
+    // Here we construct our URL
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + youtube + "&maxResults=50&key=AIzaSyDzZ7TBrC8DRq0zbEsfvPXkruckwKvYuNc";
+    var youtubeSite = "https://www.youtube.com/embed/";
 
-    console.log(searchInput);
-
-
-
-});
-
-
-
-$(document).on("click", ".dropdown-item", function () {
-    var dynamicText = $(this).text();
-
-    APIrecipe(dynamicText);
-
+    // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (youtube) {
+        //code the ajax to call a function - makes it easier to read and can be used for multiple functions
+        populate(youtube);
+        console.log(youtube);
+    });
+    //what actually gets pulled from Ajax
+    function populate(youtube) {
+        // and display it in the div with an id of youtube-view
+        // Retrieves the image
+        // Math.floor(Math.random() * youtube.items.length)
+        var random = Math.floor(Math.random() * youtube.items.length);
+        console.log(random);
+        var randomVid = youtube.items[random];
+        var video = randomVid.id.videoId;
+        // <iframe src="">;
+        // Creates an element to hold the embeded video
+        var videoDiv = $("<iframe />").attr("src", youtubeSite + video);
+        // Appends the video
+        $('.row-recipe-render').prepend(videoDiv);
+    }
 
 });
 // $.ajax({
